2f1d1db0e6087fa89009947fd2d2b8119d4ef7d7
Jonathan Chan <johnkchan252@gmail.com>
2018-08-31 04:34:49 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -62,7 +62,7 @@ class Dog
     DB[:conn].execute(sql, self.name, self.breed, self.id)
   end
   
-  def self.create(name, breed)
+  def self.create(name:, breed:)
     new_dog = Dog.new(name: name, breed: breed)
     new_dog.save
     new_dog
